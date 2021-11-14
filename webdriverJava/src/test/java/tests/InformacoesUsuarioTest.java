7e1c05787686c1e69fe86cca135de43ce0b6af36
Adolfo Agnelli <adolfoagnelli@gmail.com>
2021-11-14 19:39:00 -0300

    Add boxes and toast messagens
     Implementacao de adicionar informacoes atraves de boxes e toast messagens

--- webdriverJava/src/test/java/tests/InformacoesUsuarioTest.java
+++ webdriverJava/src/test/java/tests/InformacoesUsuarioTest.java
@@ -2,6 +2,7 @@ package tests;
 
 import static org.junit.Assert.*;
 
+import com.sun.javafx.css.CssError;
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
@@ -9,6 +10,7 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.Select;
 
 import java.util.concurrent.TimeUnit;
 
@@ -42,18 +44,45 @@ public class InformacoesUsuarioTest {
 
         // Digitar no campo com name "password" que esta dentro do formulario de id "signinbox" o texto "123456"
         formularioSignINBox.findElement(By.name("password")).sendKeys("123456");
+
         //Clicar no link com o texto "SIGN IN"
         navegador.findElement(By.linkText("SIGN IN")).click();
-        // Validar que dentro do elemento com class "me" esta o texto " Hi, Julio"
-        WebElement me = navegador.findElement(By.className("me"));
-        String textoNoElementoMe = me.getText();
-        assertEquals("Hi, Julio", textoNoElementoMe);
+
+        //Clicar em um link que possui a class "me"
+        navegador.findElement(By.className("me")).click();
+
+        //Clicar em unm link que possuii o texto "MORE DATA ABOUT YOU"
+        navegador.findElement(By.linkText("MORE DATA ABOUT YOU")).click();
+
+        //Clicar no botao atraves do seu xpath //button[@data-target="addmoredata"]
+        navegador.findElement(By.xpath("//button[@data-target=\"addmoredata\"]")).click();
+
+        //Identificar a popup onde esta o formulario de id addmoredata
+        WebElement popupAddMoreData = navegador.findElement(By.id("addmoredata"));
+
+        //Na combo de name "type" escolher a opcao "Phone"
+        WebElement campoType = popupAddMoreData.findElement(By.name("type"));
+        new Select(campoType).selectByVisibleText("Phone");
+
+        //No campo de name "contact"  digitar +5511988050796
+        popupAddMoreData.findElement(By.name("contact")).sendKeys("+5511988210565");
+
+        //Clicar no link de text "SAVE" que esta na popup
+        popupAddMoreData.findElement(By.linkText("SAVE")).click();
+
+        //Na mensagem de id "toast-container" validar que o texto e "Your contat has been added!"
+        WebElement mensagemPop = navegador.findElement(By.id("toast-container"));
+        String mensagem = mensagemPop.getText();
+        assertEquals( "Your contact has been added!", mensagem);
+
+
     }
     // Informacoes que vao ocorrer apos os testes finalizados
     @After
     public void tearDown() {
         //Fechar o navegador ( fecha todas as abas)
-        navegador.close();
+        //navegador.close();
         //Fechar todas as aba aberta {navegador.quit();
+        //test
     }
 }
