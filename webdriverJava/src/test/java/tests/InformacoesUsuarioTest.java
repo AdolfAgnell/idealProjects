fb275e20003a759ffbe300e0dc506d09d44a962f
Adolfo Agnelli <adolfoagnelli@gmail.com>
2021-11-15 01:20:52 -0300

    Add javacrip box , Webdriver wait too

--- webdriverJava/src/test/java/tests/InformacoesUsuarioTest.java
+++ webdriverJava/src/test/java/tests/InformacoesUsuarioTest.java
@@ -10,7 +10,10 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.ExpectedCondition;
+import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.Select;
+import org.openqa.selenium.support.ui.WebDriverWait;
 
 import java.util.concurrent.TimeUnit;
 
@@ -30,9 +33,7 @@ public class InformacoesUsuarioTest {
 
     // Navegando para a pagina do taskit
         navegador.get("http://www.juliodelima.com.br/taskit/");
-    }
-    @Test
-    public void testAdicionarUmaInformacaoAdicionalDoUsuario () {
+
         // Clicar no link que possui o texto "Sign in"
         navegador.findElement(By.linkText("Sign in")).click();
 
@@ -53,6 +54,9 @@ public class InformacoesUsuarioTest {
 
         //Clicar em unm link que possuii o texto "MORE DATA ABOUT YOU"
         navegador.findElement(By.linkText("MORE DATA ABOUT YOU")).click();
+    }
+    // @Test
+    public void testAdicionarUmaInformacaoAdicionalDoUsuario () {
 
         //Clicar no botao atraves do seu xpath //button[@data-target="addmoredata"]
         navegador.findElement(By.xpath("//button[@data-target=\"addmoredata\"]")).click();
@@ -70,13 +74,35 @@ public class InformacoesUsuarioTest {
         //Clicar no link de text "SAVE" que esta na popup
         popupAddMoreData.findElement(By.linkText("SAVE")).click();
 
-        //Na mensagem de id "toast-container" validar que o texto e "Your contat has been added!"
+        //Na mensagem de id "toast-container" validar que o texto e "Your contact has been added!"
         WebElement mensagemPop = navegador.findElement(By.id("toast-container"));
         String mensagem = mensagemPop.getText();
         assertEquals( "Your contact has been added!", mensagem);
 
 
     }
+
+    @Test
+    public void removerUmContatoDeUmUsuario() {
+       // CLicar no elemento pelo seu xpath //span[text()="+551166669999"]/following-sibling::a
+        navegador.findElement(By.xpath("//span[text()=\"+551166669999\"]/following-sibling::a")).click();
+
+        //Confirmar a janela javascript
+        navegador.switchTo().alert().accept();
+
+        //Validar que a mensagem apresentada foi Rest in peace, dear phone!
+        WebElement mensagemPop = navegador.findElement(By.id("toast-container"));
+        String mensagem = mensagemPop.getText();
+        assertEquals( "Rest in peace, dear phone!", mensagem);
+
+        //Aguarda ate 10 segundos para quye a janela desapareca
+        WebDriverWait aguardar = new WebDriverWait(navegador, 10);
+        aguardar.until(ExpectedConditions.stalenessOf(mensagemPop));
+
+        //Clicar no link com texto "Logout"
+        navegador.findElement(By.linkText("Logout")).click();
+    }
+
     // Informacoes que vao ocorrer apos os testes finalizados
     @After
     public void tearDown() {
